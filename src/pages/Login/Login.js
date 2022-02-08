8041ac863891ca4b5fdca145692f91436f7c5e82
NathanRamos <nata.ramos@acad.ifma.edu.br>
2022-02-08 16:16:38 -0300

    Criacao da page login e estilizacao

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,10 +1,43 @@
 import React from 'react';
+import './login.css';
+import img_apae from '../../assets/images/APAE.jpg';
 
 const Login = () => {
     return (
-        <div>
-            <h1>Login</h1>
-        </div>
+        <main className="section">
+            <div className="container">
+                <div className="card_img">
+                    <div>
+                        <img src={img_apae} alt="apae Caixas-MA"/>
+                    </div>
+
+                    <div>
+                        <p>Apae-Caxias-MA</p>
+                        <p>Sistema de login</p>
+                    </div>
+                </div>
+
+                <div className="card_login">
+                    <div>
+                        <h2>Login</h2>
+                    </div>
+
+                    <div className="inputs">
+                        <div>
+                            <input type="text" placeholder="User Name" />
+                        </div>
+                        
+                        <div>
+                            <input type="password" placeholder="Password" />
+                        </div>
+                    </div>
+                    
+                    <div>
+                        <button>Login</button>
+                    </div>
+                </div>
+            </div>
+        </main>
     );
 }
 
